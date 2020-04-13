efa6cfc1fc3064e77caea7c7b5d45f336d7631ee
Amitai Burstein <amitai@gizra.com>
2020-04-13 22:39:02 +0300

    Add deploy to Pantheon (#6)

--- RoboFile.php
+++ RoboFile.php
@@ -4,6 +4,7 @@ use ScssPhp\ScssPhp\Compiler;
 use Lurker\Event\FilesystemEvent;
 use Robo\Tasks;
 use Symfony\Component\EventDispatcher\Event;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Robo commmands.
@@ -15,6 +16,14 @@ class RoboFile extends Tasks {
   const DEV_FORMATTER = 'ScssPhp\ScssPhp\Formatter\Expanded';
 
   const THEME_BASE = 'web/themes/custom/theme_server';
+
+  /**
+   * The Pantheon name.
+   *
+   * You need to fill this information for Robo to know what's the name of your
+   * site.
+   */
+  const PANTHEON_NAME = '';
 
   /**
    * Compile the app; On success ...
@@ -163,6 +172,100 @@ class RoboFile extends Tasks {
           FilesystemEvent::ALL
         )->run();
     }
+  }
+
+  /**
+   * Deploy to Pantheon.
+   *
+   * @param string $branchName
+   *   The branch name to commit to. Default to master.
+   *
+   * @throws \Exception
+   */
+  public function deployPantheon($branchName = 'master') {
+    if (empty(self::PANTHEON_NAME)) {
+      throw new Exception('You need to fill the "PANTHEON_NAME" const in the Robo file. so it will know what is the name of your site.');
+    }
+
+    $pantheonDirectory = '.pantheon';
+
+    $result = $this
+      ->taskExec('git status -s')
+      ->printOutput(FALSE)
+      ->run();
+
+    if ($result->getMessage()) {
+      throw new Exception('The working directory is dirty. Please commit any pending changes.');
+    }
+
+    $result = $this
+      ->taskExec("cd $pantheonDirectory && git status -s")
+      ->printOutput(FALSE)
+      ->run();
+
+    if ($result->getMessage()) {
+      throw new Exception('The Pantheon directory is dirty. Please commit any pending changes.');
+    }
+
+    // Validate pantheon.yml has web_docroot: true
+    if (!file_exists($pantheonDirectory . '/pantheon.yml')) {
+      throw new Exception("pantheon.yml is missing from the Pantheon directory ($pantheonDirectory)");
+    }
+
+    $yaml = Yaml::parseFile($pantheonDirectory . '/pantheon.yml');
+    if (empty($yaml['web_docroot'])) {
+      throw new Exception("'web_docroot: true' is missing from pantheon.yml in Pantheon directory ($pantheonDirectory)");
+    }
+
+    $this->_exec("cd $pantheonDirectory && git checkout $branchName");
+
+    // Compile theme
+    $this->compileTheme();
+
+    $rsyncExclude = [
+      '.git',
+      '.ddev',
+      '.idea',
+      '.pantheon',
+      'sites/default',
+      'pantheon.yml',
+      'pantheon.upstream.yml',
+    ];
+
+    $rsyncExcludeString = '--exclude=' . join(' --exclude=', $rsyncExclude);
+
+    // Copy all files and folders.
+    $this->_exec("rsync -az --progress --delete $rsyncExcludeString . $pantheonDirectory");
+
+    // We don't want to change Pantheon's git ignore, as we do want to commit
+    // vendor and contrib directories.
+    // @todo: Ignore it from rsync, but './.gitignore' didn't work.
+    $this->_exec("cd $pantheonDirectory && git checkout .gitignore");
+
+    $this->_exec("cd $pantheonDirectory && git status");
+
+    $commitAndDeployConfirm = $this->confirm('Commit changes and deploy?');
+    if (!$commitAndDeployConfirm) {
+      $this->say('Aborted commit and deploy, you can do it manually');
+      return;
+    }
+
+    $pantheonName = self::PANTHEON_NAME;
+    $pantheonTerminusEnvironment = $pantheonName . '.dev';
+
+    $this
+      ->taskExecStack()
+      ->exec("cd $pantheonDirectory && git pull && git add . && git commit -am 'Site update' && git push")
+      ->exec("terminus remote:drush $pantheonTerminusEnvironment -- cr")
+
+      // A second cache-clear, because Drupal...
+      ->exec("terminus remote:drush $pantheonTerminusEnvironment -- cr")
+      ->exec("terminus remote:drush $pantheonTerminusEnvironment -- updb -y")
+
+      // A second config import, because Drupal...
+      ->exec("terminus remote:drush $pantheonTerminusEnvironment -- cim -y")
+      ->exec("terminus remote:drush $pantheonTerminusEnvironment -- cim -y")
+      ->run();
   }
 
 }
