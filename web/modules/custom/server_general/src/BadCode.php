75c992f573d31527734a6536c9770588006c7db9
Mariano D'Agostino <mariano@gizra.com>
2020-07-16 16:53:14 -0300

    Fix name of the class

--- web/modules/custom/server_general/src/BadCode.php
+++ web/modules/custom/server_general/src/BadCode.php
@@ -5,13 +5,13 @@ namespace Drupal\server_general;
 /**
  * This file looks fine, but has a syntax error.
  */
-class ServerGeneralExampleTest extends ExistingSiteBase {
+class BadCode extends ExistingSiteBase {
 
   /**
    * An example of a syntax error.
    */
   public function badCode() {
-    $oh_no
+    $oh_no = 1
   }
 
 }
