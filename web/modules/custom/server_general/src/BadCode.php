bfeb3bb794378aec8533696e95fa65f615a6210e
Mariano D'Agostino <mariano@gizra.com>
2020-07-16 16:49:59 -0300

    Trigger a syntax error

---
+++ web/modules/custom/server_general/src/BadCode.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\server_general;
+
+/**
+ * This file looks fine, but has a syntax error.
+ */
+class ServerGeneralExampleTest extends ExistingSiteBase {
+
+  /**
+   * An example of a syntax error.
+   */
+  public function badCode() {
+    $oh_no
+  }
+
+}
