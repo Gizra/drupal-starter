08446c65626f95c2abf25980b3f766381f2084cd
Amitai Burstein <amitai@gizra.com>
2020-08-05 12:57:56 +0300

    Remove unsued themes, and add theme for tag

--- web/modules/custom/server_general/src/TagBuilderTrait.php
+++ web/modules/custom/server_general/src/TagBuilderTrait.php
@@ -19,18 +19,15 @@ trait TagBuilderTrait {
    *   The term to render.
    *
    * @return array
-   *   The renderable array.
+   *   Render array.
    */
   public function buildTag(TermInterface $term) {
-    $classes = 'mr-1 text-ms px-3 py-1 my-1 text-center leading-normal rounded-large border-2 border-purple-primary hover:text-blue-900 hover:border-blue-900 rounded-md text-purple-primary h-8 overflow-hidden';
-
     return [
-      '#type' => 'link',
+      '#theme' => 'server_theme_tag',
       '#title' => $term->label(),
       // As the style guide is using this with mocked terms (i.e. terms which
       // are not saved), we fallback to a link to the homepage.
       '#url' => !$term->isNew() ? $term->toUrl() : Url::fromRoute('<front>'),
-      '#attributes' => ['class' => explode(' ', $classes)],
     ];
   }
 
