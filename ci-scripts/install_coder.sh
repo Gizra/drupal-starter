bc66fd3019aa896e2658d012f3ab3eb7e902ba01
Aron Novak <AronNovak@users.noreply.github.com>
2020-09-08 10:01:19 +0200

    Update ci-scripts/install_coder.sh
    Co-authored-by: Amitai Burstein <amitai@gizra.com>

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -8,5 +8,5 @@ set -e
 # ---------------------------------------------------------------------------- #
 
 cd "$TRAVIS_BUILD_DIR"
-COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:8.3.10
+COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:^8.3.10
 phpcs --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer
