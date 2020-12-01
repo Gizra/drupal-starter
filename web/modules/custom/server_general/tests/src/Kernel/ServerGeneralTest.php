25a8946cfaea0a69c9d7a613736668c6d7d33adc
Mariano D'Agostino <mariano@gizra.com>
2020-12-01 10:24:51 -0300

    Fix coding standards

--- web/modules/custom/server_general/tests/src/Kernel/ServerGeneralTest.php
+++ web/modules/custom/server_general/tests/src/Kernel/ServerGeneralTest.php
@@ -5,10 +5,15 @@ namespace Drupal\Tests\server_general\Kernel;
 use Drupal\KernelTests\KernelTestBase;
 
 /**
- * Server general test functionality
+ * Server general test functionality.
  */
 class ServerGeneralTest extends KernelTestBase {
 
+  /**
+   * Modules to install for this test.
+   *
+   * @var array
+   */
   public static $modules = ['server_general'];
 
   /**
