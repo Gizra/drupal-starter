214ba94acf17e2ab62c6aa11b04949e69e858b18
Aron Novak <aron@gizra.com>
2021-02-20 05:57:54 +0100

    also do not install docker-compose, the docs states that it's available

--- ci-scripts/install_ddev.sh
+++ ci-scripts/install_ddev.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# -------------------------------------------------- #
-# Installing ddev dependencies.
-# -------------------------------------------------- #
-echo "Install Docker Compose."
-sudo rm /usr/local/bin/docker-compose
-curl -s -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" > docker-compose
-chmod +x docker-compose
-sudo mv docker-compose /usr/local/bin
-
 echo "Install mkcert."
 wget -nv https://github.com/FiloSottile/mkcert/releases/download/v1.4.0/mkcert-v1.4.0-linux-amd64
 sudo mv mkcert-v1.4.0-linux-amd64 /usr/bin/mkcert
@@ -19,9 +10,6 @@ mkcert -install
 echo "Install ddev."
 curl -s -L https://raw.githubusercontent.com/drud/ddev/master/scripts/install_ddev.sh | bash
 
-# -------------------------------------------------- #
-# Configuring ddev.
-# -------------------------------------------------- #
 echo "Configuring ddev."
 mkdir ~/.ddev
 cp "ci-scripts/global_config.yaml" ~/.ddev/
