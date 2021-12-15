63308923afe039cd8ea6396d1b850098c8594665
Amitai Burstein <amitai@gizra.com>
2021-12-15 15:00:24 +0200

    Upgrade Tailwind to V3

--- web/themes/custom/server_theme/tailwind.config.js
+++ web/themes/custom/server_theme/tailwind.config.js
@@ -1,7 +1,6 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-  mode: 'jit',
   theme: {
     extend: {
     },
@@ -10,23 +9,19 @@ module.exports = {
       'body': ["Open Sans", 'sans-serif'],
     }
   },
-  purge: {
-    content: [
-      // Look in the twig files.
-      './templates/**/*.html.twig',
-      // A preprocess function might inject a class.
-      './server_theme.theme',
-      // Custom module and the Style guide may have needed classes.
-      '../../../modules/custom/**/*.php',
-      '../../../modules/custom/**/*.html.twig',
-    ],
-    options: {
-      safelist: [
-        // Add here custom class names. Since we're using TW's jit (Just-In-
-        // Time), `safelist` must be full class names, and not regex.
-      ],
-    },
-  },
+  content: [
+    // Look in the twig files.
+    './templates/**/*.html.twig',
+    // A preprocess function might inject a class.
+    './server_theme.theme',
+    // Custom module and the Style guide may have needed classes.
+    '../../../modules/custom/**/*.php',
+    '../../../modules/custom/**/*.html.twig',
+  ],
+  safelist: [
+    // Add here custom class names.
+    // https://tailwindcss.com/docs/content-configuration#safelisting-classes
+  ],
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
