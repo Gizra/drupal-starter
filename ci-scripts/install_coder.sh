7bd06ef8ade0add993d1a684c59ef7325bffc977
Aron Novak <aron@gizra.com>
2022-01-31 08:09:13 +0100

    use absolute path

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -10,4 +10,4 @@ set -e
 cd "$TRAVIS_BUILD_DIR"
 COMPOSER_MEMORY_LIMIT=-1 composer global require slevomat/coding-standard:^7.0
 COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:^8.3.10
-phpcs --config-set installed_paths ~/.config/composer/vendor/drupal/coder/coder_sniffer,~/.config/composer/vendor/slevomat/coding-standard
+phpcs --config-set installed_paths $(readlink -f ~/.config/composer/vendor/drupal/coder/coder_sniffer),$(readlink -f ~/.config/composer/vendor/slevomat/coding-standard)
