09077c20cfe54b4f66a6cf3260e7745ce8c831a4
Mariano D'Agostino <mariano@gizra.com>
2022-02-07 11:20:44 -0300

    Install coder 8.3.14 following latest config instructions

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -8,6 +8,4 @@ set -e
 # ---------------------------------------------------------------------------- #
 
 cd "$TRAVIS_BUILD_DIR"
-COMPOSER_MEMORY_LIMIT=-1 composer global require slevomat/coding-standard:^7.0
-COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:^8.3.10
-phpcs --config-set installed_paths "$(readlink -f ~/.config/composer/vendor/drupal/coder/coder_sniffer)","$(readlink -f ~/.config/composer/vendor/slevomat/coding-standard)"
+COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:^8.3.14
