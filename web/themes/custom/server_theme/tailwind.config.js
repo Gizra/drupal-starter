c30c4b1ddcde8334c0b710e5954c00746af6b285
Amitai Burstein <amitai@gizra.com>
2022-02-07 15:51:09 +0200

    Prettier fixes

--- web/themes/custom/server_theme/tailwind.config.js
+++ web/themes/custom/server_theme/tailwind.config.js
@@ -1,29 +1,25 @@
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   theme: {
-    extend: {
-    },
+    extend: {},
     fontFamily: {
-      'headers': ["Roboto", 'sans-serif'],
-      'body': ["Open Sans", 'sans-serif'],
-    }
+      headers: ["Roboto", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
   },
   content: [
     // Look in the twig files.
-    './templates/**/*.html.twig',
+    "./templates/**/*.html.twig",
     // A preprocess function might inject a class.
-    './server_theme.theme',
+    "./server_theme.theme",
     // Custom module and the Style guide may have needed classes.
-    '../../../modules/custom/**/*.php',
-    '../../../modules/custom/**/*.html.twig',
+    "../../../modules/custom/**/*.php",
+    "../../../modules/custom/**/*.html.twig",
   ],
   safelist: [
     // Add here custom class names.
     // https://tailwindcss.com/docs/content-configuration#safelisting-classes
   ],
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
-  ],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/line-clamp")],
 };
