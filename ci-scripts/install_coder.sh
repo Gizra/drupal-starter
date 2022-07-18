446ab924e7ee9018edddf10e4b9ce0bf9cddf111
Aron Novak <aron@gizra.com>
2022-07-18 13:12:13 +0200

    unblock travis

--- ci-scripts/install_coder.sh
+++ ci-scripts/install_coder.sh
@@ -9,3 +9,4 @@ set -e
 
 cd "$TRAVIS_BUILD_DIR"
 COMPOSER_MEMORY_LIMIT=-1 composer global require drupal/coder:^8.3.14
+composer global config --no-plugins allow-plugins.dealerdirect/phpcodesniffer-composer-installer true
