32ae6f1ebf0503d99c994457ffe2598341c255d6
Bysa Boro <baysaa@gmail.com>
2022-09-15 15:27:51 +0100

    Cleanup and use dependency injection

--- web/modules/custom/server_general/src/Commands/ServerGeneralCommands.php
+++ web/modules/custom/server_general/src/Commands/ServerGeneralCommands.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Drupal\server_general\Commands;
 
-use Consolidation\OutputFormatters\StructuredData\RowsOfFields;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\node\NodeInterface;
 use Drush\Commands\DrushCommands;
 
@@ -10,6 +13,34 @@ use Drush\Commands\DrushCommands;
  * Server General Drush commands.
  */
 class ServerGeneralCommands extends DrushCommands {
+
+  /**
+   * Entity Type Manager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * Config Factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected ConfigFactoryInterface $configFactory;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory service.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, ConfigFactoryInterface $config_factory) {
+    parent::__construct();
+    $this->entityTypeManager = $entity_type_manager;
+    $this->configFactory = $config_factory;
+  }
 
   /**
    * Command description here.
@@ -20,57 +51,28 @@ class ServerGeneralCommands extends DrushCommands {
    * @command server_general:set-homepage
    * @aliases set-homepage
    */
-  public function setHomepageAfterInstall() {
-    /** @var NodeInterface[] $homepage */
-    $homepage = \Drupal::entityTypeManager()->getStorage('node')->loadByProperties([
+  public function setHomepageAfterInstall(): void {
+    /** @var \Drupal\node\NodeInterface[] $homepage */
+    $homepage = $this->entityTypeManager->getStorage('node')->loadByProperties([
       'title' => 'Homepage',
       'type' => 'landing_page',
       'status' => NodeInterface::PUBLISHED,
     ]);
+    /** @var \Consolidation\Log\Logger $logger */
+    $logger = $this->logger();
     if (empty($homepage)) {
-      $this->logger()->error(dt('Unable to find any published landing_page nodes titled "Homepage".'));
+      $logger->error(dt('Unable to find any published landing_page nodes titled "Homepage".'));
       return;
     }
 
     $homepage = reset($homepage);
     $front = "/node/{$homepage->id()}";
-    $config = \Drupal::configFactory()->getEditable('system.site');
+    $config = $this->configFactory->getEditable('system.site');
     $config->set('page.front', $front);
     $config->save();
-
-    $this->logger()->success(dt('Homepage set to @front.', [
+    $logger->success(dt('Homepage set to @front.', [
       '@front' => $front,
     ]));
   }
 
-  /**
-   * An example of the table output format.
-   *
-   * @param array $options An associative array of options whose values come from cli, aliases, config, etc.
-   *
-   * @field-labels
-   *   group: Group
-   *   token: Token
-   *   name: Name
-   * @default-fields group,token,name
-   *
-   * @command server_general:token
-   * @aliases token
-   *
-   * @filter-default-field name
-   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
-   */
-  public function token($options = ['format' => 'table']) {
-    $all = \Drupal::token()->getInfo();
-    foreach ($all['tokens'] as $group => $tokens) {
-      foreach ($tokens as $key => $token) {
-        $rows[] = [
-          'group' => $group,
-          'token' => $key,
-          'name' => $token['name'],
-        ];
-      }
-    }
-    return new RowsOfFields($rows);
-  }
 }
