28913dabcb9b555ea10aa5aba8171b4948123447
Aron Novak <aron@gizra.com>
2022-10-05 11:57:22 +0200

    do not cache phpunit result

--- ci-scripts/test_phpunit.sh
+++ ci-scripts/test_phpunit.sh
@@ -4,6 +4,6 @@ set -e
 # -------------------------------------------------- #
 # Run PHPUnit tests
 # -------------------------------------------------- #
-ddev phpunit --testdox
+ddev phpunit --do-not-cache-result --testdox
 
 exit 0
