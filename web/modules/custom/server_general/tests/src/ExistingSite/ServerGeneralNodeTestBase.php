73205039f51468221454b336310a3a8637a020e1
Bysa Boro <baysaa@gmail.com>
2022-10-24 13:59:50 +0100

    Add paragraph reference values extractor

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralNodeTestBase.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralNodeTestBase.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\server_general\ExistingSite;
 
+use Drupal\paragraphs\ParagraphInterface;
+
 /**
  * Abstract class to hold shared logic to check various content types.
  */
@@ -12,6 +14,23 @@ abstract class ServerGeneralNodeTestBase extends ServerGeneralFieldableEntityTes
    */
   public function getEntityType(): string {
     return 'node';
+  }
+
+  /**
+   * Extract the reference values for a paragraph.
+   *
+   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
+   *   The paragraph.
+   *
+   * @return array
+   *   Reference values of the given paragraph containing target_id and
+   *   target_revision_id keys.
+   */
+  protected function getParagraphReferenceValues(ParagraphInterface $paragraph) {
+    return [
+      'target_id' => $paragraph->id(),
+      'target_revision_id' => $paragraph->getRevisionId(),
+    ];
   }
 
 }
