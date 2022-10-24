7b18b9136237be334c6b14702b165ebf2f390b97
Bysa Boro <baysaa@gmail.com>
2022-10-24 14:01:30 +0100

    Move the function to FieldableENtityBase

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralNodeTestBase.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralNodeTestBase.php
@@ -16,21 +16,4 @@ abstract class ServerGeneralNodeTestBase extends ServerGeneralFieldableEntityTes
     return 'node';
   }
 
-  /**
-   * Extract the reference values for a paragraph.
-   *
-   * @param \Drupal\paragraphs\ParagraphInterface $paragraph
-   *   The paragraph.
-   *
-   * @return array
-   *   Reference values of the given paragraph containing target_id and
-   *   target_revision_id keys.
-   */
-  protected function getParagraphReferenceValues(ParagraphInterface $paragraph) {
-    return [
-      'target_id' => $paragraph->id(),
-      'target_revision_id' => $paragraph->getRevisionId(),
-    ];
-  }
-
 }
