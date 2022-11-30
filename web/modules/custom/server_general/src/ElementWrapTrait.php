b773337d77510a6f46d88c13453ba9f072e87eb0
Dipak Yadav <dipakmdhrm@gmail.com>
2022-11-30 11:39:26 +0530

    Update comments.

--- web/modules/custom/server_general/src/ElementWrapTrait.php
+++ web/modules/custom/server_general/src/ElementWrapTrait.php
@@ -180,8 +180,8 @@ trait ElementWrapTrait {
    * @param bool $is_italic
    *   TRUE to make the text italic.
    * @param string|null $font_size
-   *   The font size. Can be `sm` or `lg`. Defaults to NULL, which will not
-   *   change the font size.
+   *   The font size. Can be `sm`, `lg` or xl. Defaults to NULL, which will
+   *   not change the font size.
    *
    * @return array
    *   Render array.
