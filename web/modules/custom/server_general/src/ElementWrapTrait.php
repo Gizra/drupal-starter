b14569834dc44cfbbaf4e7fd398e20fef1ff1a3d
Amitai Burstein <amitai@gizra.com>
2022-12-05 19:32:41 +0200

    Improve array filtering

--- web/modules/custom/server_general/src/ElementWrapTrait.php
+++ web/modules/custom/server_general/src/ElementWrapTrait.php
@@ -14,7 +14,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerWide(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -33,7 +33,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerNarrow(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -55,7 +55,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerVerticalSpacing(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -77,7 +77,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerVerticalSpacingTiny(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -99,7 +99,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerVerticalSpacingBig(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -121,7 +121,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerVerticalSpacingHuge(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -143,7 +143,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapContainerBottomPadding(array $element): array {
-    $element = array_filter($element);
+    $element = $this->filterEmptyElements($element);
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -162,7 +162,7 @@ trait ElementWrapTrait {
    *   Render array.
    */
   protected function wrapProseText(array $element): array {
-    $element = array_filter($element);
+
     if (empty($element)) {
       // Element is empty, so no need to wrap it.
       return [];
@@ -224,6 +224,27 @@ trait ElementWrapTrait {
       '#theme' => 'server_theme_container_rounded_corners_big',
       '#items' => $element,
     ];
+  }
+
+  /**
+   * Remove nested empty arrays.
+   *
+   * If the element is an array of arrays, we'd like to remove empty ones.
+   * However, if the element is a one dimension array, we'll skip it.
+   *
+   * @param array $element
+   *   The render array.
+   *
+   * @return array
+   *   The filtered render array.
+   */
+  protected function filterEmptyElements(array $element): array {
+    if (count($element) <= 1) {
+      // No change.
+      return $element;
+    }
+
+    return array_filter($element);
   }
 
 }
