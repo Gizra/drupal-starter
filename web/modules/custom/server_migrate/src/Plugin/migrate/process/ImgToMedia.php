301629549bd22d24b81444e2a5822dfd7503eb75
Bysa Boro <baysaa@gmail.com>
2022-12-05 12:21:42 +0000

    Change drupal-entity to drupal-media in migrations

--- web/modules/custom/server_migrate/src/Plugin/migrate/process/ImgToMedia.php
+++ web/modules/custom/server_migrate/src/Plugin/migrate/process/ImgToMedia.php
@@ -23,11 +23,10 @@ use Symfony\Component\Mime\MimeTypeGuesserInterface;
  * <img src="/sites/default/files/path/to/file.ext" />
  *
  * # To this
- * <drupal-entity
- *   data-embed-button="media"
- *   data-entity-embed-display="view_mode:media.full"
+ * <drupal-media
+ *   data-align="center"
  *   data-entity-type="media"
- *   data-entity-id="1234"></drupal-entity>
+ *   data-entity-uuid="1234"></drupal-media>
  * @endcode
  *
  * @MigrateProcessPlugin(
