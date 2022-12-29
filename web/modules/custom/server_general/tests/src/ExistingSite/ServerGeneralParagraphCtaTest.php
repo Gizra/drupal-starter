93e04b10561ccf53f0f0de60562a1ed628f28d0e
Amitai Burstein <amitai@gizra.com>
2022-12-29 23:31:49 +0200

    Fix tests

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralParagraphCtaTest.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralParagraphCtaTest.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\server_general\ExistingSite;
 
+use Drupal\paragraphs\Entity\Paragraph;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Test 'cta' paragraph type.
  */
@@ -30,6 +33,42 @@ class ServerGeneralParagraphCtaTest extends ServerGeneralParagraphTestBase {
    */
   public function getOptionalFields(): array {
     return [];
+  }
+
+  /**
+   * Test render of the paragraph.
+   */
+  public function testRender() {
+    $cta = Paragraph::create(['type' => 'cta']);
+    $cta->set('field_title', 'Lorem ipsum dolor sit amet');
+    $cta->set('field_subtitle', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+    $cta->set('field_link', [
+      'uri' => 'https://example.com',
+      'title' => 'Button text',
+    ]);
+    $cta->save();
+    $this->markEntityForCleanup($cta);
+
+    $user = $this->createUser();
+    $node = $this->createNode([
+      'title' => 'Landing Page',
+      'type' => 'landing_page',
+      'uid' => $user->id(),
+      'field_paragraphs' => [
+        $this->getParagraphReferenceValues($cta),
+      ],
+    ]);
+    $node->setPublished()->save();
+    $this->assertEquals($user->id(), $node->getOwnerId());
+
+    $this->drupalGet($node->toUrl());
+    $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
+
+    $this->assertSession()->elementTextContains('css', '.cta', 'Lorem ipsum dolor sit amet');
+    $this->assertSession()->elementTextContains('css', '.cta', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+    $this->assertSession()->elementTextContains('css', '.cta', 'Button text');
+    $this->assertSession()->linkExists('Button text');
+    $this->assertSession()->linkByHrefExists('https://example.com');
   }
 
 }
