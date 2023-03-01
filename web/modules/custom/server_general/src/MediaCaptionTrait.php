629039eb3dce33bb07807511909d5d1fe094586f
Zsolt Nagy <balagan73@gmail.com>
2023-03-01 19:24:30 +0100

    Adding alignment and credit fields to MediaCaptionTrait.

--- web/modules/custom/server_general/src/MediaCaptionTrait.php
+++ web/modules/custom/server_general/src/MediaCaptionTrait.php
@@ -21,19 +21,27 @@ trait MediaCaptionTrait {
    *   The entity.
    * @param string $field_name
    *   The field name. Defaults to `field_caption`.
+   * @param string $credit_field_name
+   *   The credit field name. Default: 'field_photo_credit'.
+   * @param string $caption_alignment
+   *   The caption alignment. Default: 'start'.
    *
    * @return array
    *   Render array.
    */
-  public function buildCaption(MediaInterface $entity, string $field_name = 'field_caption'): array {
+  public function buildCaption(MediaInterface $entity, string $field_name = 'field_caption', string $credit_field_name = 'field_photo_credit', string $caption_alignment = 'start'): array {
     $caption = $this->getTextFieldValue($entity, $field_name);
-    if (empty($caption)) {
+    $credit = $entity->hasField($credit_field_name) ? $this->getTextFieldValue($entity, $credit_field_name) : NULL;
+    if (empty($caption) && empty($credit)) {
+      // Nothing to output.
       return [];
     }
 
     return [
       '#theme' => 'server_theme_media_caption',
       '#caption' => $caption,
+      '#credit' => $credit,
+      '#caption_alignment' => $caption_alignment,
     ];
   }
 
