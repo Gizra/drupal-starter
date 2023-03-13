82f8c666d0cbeae67de9b66a0d0906d707ac6fe8
Aron Novak <aron@gizra.com>
2023-03-13 11:30:28 +0100

    paragraph translation

---
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralParagraphTranslatability.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\Tests\server_general\ExistingSite;
+
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Tests entity references.
+ */
+class ServerGeneralParagraphTranslatability extends ExistingSiteBase {
+
+  /**
+   * Checks all entity reference fields to nodes use the select2 widget.
+   */
+  public function testParagraphsTranslation() {
+    $em = \Drupal::entityTypeManager();
+    $field_configs = $em->getStorage('field_config')->loadMultiple();
+
+    foreach ($field_configs as $id => $field_config) {
+      if ($field_config->getType() !== 'entity_reference_revisions') {
+        continue;
+      }
+
+      $storage_definition = $field_config->getFieldStorageDefinition();
+      $referencing = $storage_definition->getSetting('target_type');
+      if ($referencing != 'paragraph') {
+        continue;
+      }
+
+      $this->assertFalse($field_config->isTranslatable(), sprintf("%s must not be translatable", $id));
+    }
+  }
+
+}
