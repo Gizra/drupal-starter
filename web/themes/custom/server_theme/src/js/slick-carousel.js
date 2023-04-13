4b73c9934770f37ff616d787885a1554bab0f138
Amitai Burstein <amitai@gizra.com>
2023-04-13 22:38:49 +0300

    Convert Slick to SwiperJS

--- web/themes/custom/server_theme/src/js/slick-carousel.js
+++ web/themes/custom/server_theme/src/js/slick-carousel.js
@@ -8,7 +8,7 @@
     // All the elements in the page that need a Slick carousel. It may be
     // a carousel that is always attached, or one that is attached only on
     // mobile.
-    const $elements = $('.carousel-wrapper');
+    const $elements = $('.swiper');
 
     /**
      * Determine if Slick is already attached.
@@ -16,7 +16,7 @@
      * @returns {boolean|jQuery}
      */
     function hasSlickAttached($element) {
-      return $element.hasClass('slick-slider');
+      return $element.hasClass('swiper-slide');
     }
 
     /**
@@ -30,20 +30,6 @@
       if (!$elements.length) {
         // No carousels needed on page.
         return;
-      }
-
-      /**
-       * Get the total slides count of a carousel of a given element.
-       *
-       * @param $element
-       *   The given element.
-       */
-      function getTotalCardsCount($element) {
-        if (hasSlickAttached($element)) {
-          // Don't count cloned slides.
-          return $element.find('.slick-slide').not('.slick-cloned').length;
-        }
-        return $element.find('.carousel-slide').length;
       }
 
       $elements.each(function () {
@@ -63,98 +49,30 @@
         const $prevArrow = $parent.find('.slick-arrow-prev');
         const $nextArrow = $parent.find('.slick-arrow-next');
 
-        // Get the number of total cards we have.
-        const numSlides = getTotalCardsCount($element);
-        if (!numSlides) {
-          console.error('Slides are missing the `carousel-slide` class');
-          return;
-        }
-        if (hasSlickAttached($element)) {
-          // We have already attached Slick.
-          return;
-        }
+        const swiper = new Swiper('.swiper', {
+          // Optional parameters
+          direction: 'horizontal',
+          loop: true,
 
-        // We'd like to show only a single slide on mobile.
-        let slidesToShow;
+          // If we need pagination
+          pagination: {
+            el: '.swiper-pagination',
+          },
 
-        if (singleSlide) {
-          slidesToShow = 1;
-        }
-        else if (isResponsive) {
-          slidesToShow = 1;
-        }
-        else {
-          slidesToShow = numSlides > 3 ? 3 : numSlides;
-        }
+          // Navigation arrows
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
 
-        const config = {
-          infinite: true,
-          // Set the direction based on the current language.
-          rtl: drupalSettings.language.direction === 'rtl',
-          focusOnSelect: false,
-          arrows: showArrows,
-          mobileFirst: true,
-          slidesToShow: slidesToShow,
-          slidesToScroll: slidesToScroll || slidesToShow,
-          dots: showDots,
-          variableWidth: variableWidth,
-          // This defaults to 1 in slick, which in conjunction with slidesPerRow
-          // setting, also default to 1, initializes Grid Mode on Slick. We
-          // set rows to 0 to disable grid mode, which adds a lot of extra
-          // unwanted markup that adds 'display:inline-block' and ends up
-          // creating extra whitespace.
-          // @see https://github.com/kenwheeler/slick/issues/3581.
-          rows: 0,
-          centerMode: centerMode,
-          centerPadding: hasPadding ? '10px' : '0',
-        };
+          // And if we need scrollbar
+          scrollbar: {
+            el: '.swiper-scrollbar',
+          },
+        });
 
-        if (isResponsive) {
-          // Add the responsive config.
-          config.responsive = [
-            {
-              breakpoint: 1279,
-              settings: {
-                slidesToShow: slidesToShowDesktop,
-                slidesToScroll: slidesToShowDesktop,
-                arrows: showArrows,
-                prevArrow: $prevArrow,
-                nextArrow: $nextArrow,
-                variableWidth: variableWidth,
-                centerPadding: hasPadding ? '150px' : '0',
-              },
-            },
-            {
-              breakpoint: 1023,
-              settings: {
-                slidesToShow: slidesToShowLaptop,
-                slidesToScroll: slidesToShowLaptop,
-                arrows: showArrows,
-                prevArrow: $prevArrow,
-                nextArrow: $nextArrow,
-                variableWidth: variableWidth,
-                centerPadding: hasPadding ? '100px' : '0',
-              },
-            },
-            {
-              breakpoint: 639,
-              settings: {
-                slidesToShow: slidesToShowTablet,
-                slidesToScroll: slidesToShowTablet,
-                arrows: showArrows,
-                variableWidth: variableWidth,
-                centerPadding: hasPadding ? '50px' : '0',
-              },
-            }
-          ];
-        }
+        console.log(swiper);
 
-        if (showArrows) {
-          config.prevArrow = $prevArrow;
-          config.nextArrow = $nextArrow;
-        }
-
-        $element.slick(config);
       });
     }
 
@@ -163,7 +81,8 @@
 
     // Listen on window resize event.
     $(window).resize(function () {
-      attachedOrDestroySlick();
+      // @todo: Keep?
+      // attachedOrDestroySlick();
     });
   });
 
