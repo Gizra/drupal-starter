22207b0895da8b4bb7da1eb2535743f9b703f764
Aron Novak <aron@gizra.com>
2023-04-19 11:52:43 +0200

    test for es views caching config

---
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralCacheTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Drupal\views\Entity\View;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+use Drupal\views\Views;
+
+/**
+ * Cache-related assertions.
+ */
+class ServerGeneralCacheTest extends ExistingSiteBase {
+
+  /**
+   * Test Search API-based views, they should use Search API tag-based caching.
+   */
+  public function testSearchApiBasedViewsCache() {
+    // Load all views definition.
+    $views = Views::getAllViews();
+
+    // Iterate through each view.
+    foreach ($views as $view) {
+      // Check if the view uses the Search API.
+      $this->checkViewForSearchApiTagBasedCaching($view);
+    }
+  }
+
+  /**
+   * Checks if the given view uses the Search API tag-based caching.
+   *
+   * @param \Drupal\views\Entity\View $view
+   *   The view to check.
+   */
+  protected function checkViewForSearchApiTagBasedCaching(View $view) {
+    if ($view->get('base_field') != 'search_api_id') {
+      return;
+    }
+    $displays = $view->get('display');
+    foreach ($displays as $display) {
+      if (!isset($display['display_options']['cache'])) {
+        continue;
+      }
+      $this->assertEquals('search_api_tag', $display['display_options']['cache']['type']);
+    }
+  }
+
+}
