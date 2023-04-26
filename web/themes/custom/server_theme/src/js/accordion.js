dbeffc7f88202ae9523b3f739e132a184da82907
Aron Novak <AronNovak@users.noreply.github.com>
2023-04-26 07:35:10 +0000

    Accordion bugfix backport (#499)
    Co-authored-by: mgurjanov <milan.gurjanov@gizra.com>

--- web/themes/custom/server_theme/src/js/accordion.js
+++ web/themes/custom/server_theme/src/js/accordion.js
@@ -10,26 +10,16 @@
       $('.accordion .accordion-title').once().click(function() {
         const $this = $(this);
         const $target = $this.next();
-        // Indicate if description is hidden.
-        const isHidden = $target.hasClass('hidden');
 
         // Show or hide the description.
-        if (isHidden) {
-          $target.slideDown();
-          $target.removeClass('hidden');
-        }
-        else {
-          $target.slideUp();
-          $target.addClass('hidden');
-        }
+        $target.slideToggle();
+        $target.toggleClass('hidden');
 
         // Change the SVG icon.
-        const hideSvgClassName = isHidden ? 'minus-circle' : 'plus-circle';
-        const showSvgClassName = isHidden ? 'plus-circle' : 'minus-circle';
-        $this.find('.' + hideSvgClassName).toggleClass('hidden');
-        $this.find('.' + showSvgClassName).toggleClass('hidden');
-
+        $this.find('.minus-circle').toggleClass('hidden');
+        $this.find('.plus-circle').toggleClass('hidden');
       });
+
     }
   }
 })(jQuery, Drupal);
