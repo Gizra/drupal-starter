3f35fa4477ccc5be27026e8199169f000bd74c43
Mariano D'Agostino <mariano@gizra.com>
2023-12-29 16:52:48 -0300

    Update code

--- web/sites/default/settings.docker_production.php
+++ web/sites/default/settings.docker_production.php
@@ -19,11 +19,6 @@ $databases['default']['default'] = [
 
 $settings['hash_salt'] = 'ETXSRhodvuWLJsBUnpgkRpTXOLqbuozKXwjwZkuGiHSCpdEQLHXgdgGUHeCVHnXv';
 
-// UNDCO <-> UNSDG content sharing.
-$settings['unsdg_content_sync.domain'] = $_ENV['docker_production_unsdg_content_domain'] ?: 'https://unsdg.un.org';
-$settings['unsdg_content_sync.jsonapi_user'] = $_ENV['docker_production_unsdg_content_domain'] ?: 'jsonapi.client';
-$settings['unsdg_content_sync.jsonapi_pass'] = $_ENV['docker_production_unsdg_content_sync_password'] ?: '1234';
-
 // Custom settings.
 $settings['file_private_path'] = '/app/private';
 $settings['config_sync_directory'] = '../config/sync';
@@ -32,10 +27,9 @@ $settings['config_sync_directory'] = '../config/sync';
 $settings['container_yamls'][] = 'modules/contrib/redis/example.services.yml';
 $settings['container_yamls'][] = 'modules/contrib/redis/redis.services.yml';
 $settings['redis.connection']['interface'] = 'PhpRedis'; // Can be "Predis".
-$settings['redis.connection']['host']      = $_ENV['docker_production_redis_host'] ?: 'undco-redis';
+$settings['redis.connection']['host']      = $_ENV['docker_production_redis_host'] ?: 'starter-redis';
 $settings['cache']['default'] = 'cache.backend.redis';
 
 $settings['trusted_host_patterns'] = [
   'localhost',
-  '^www\.un-dco\.org$',
 ];
