49a5765c8849398f5f45323d188d12c9a5339bcf
Dipak Yadav <dipakmdhrm@gmail.com>
2024-07-16 13:05:02 +0530

    Always backup prod before deployment.

--- robo-components/DeploymentTrait.php
+++ robo-components/DeploymentTrait.php
@@ -385,6 +385,9 @@ trait DeploymentTrait {
       ->stopOnFail();
 
     if ($do_deploy) {
+      if ($env == 'live') {
+        $task->exec("terminus backup:create $pantheon_terminus_environment");
+      }
       $task->exec("terminus env:deploy $pantheon_terminus_environment");
     }
 
