ca31e2d6c7d3ae6d987e6b34b5e235321b8c81f4
Aron Novak <aron@gizra.com>
2024-08-02 14:25:33 +0200

    update

--- robo-components/DeploymentTrait.php
+++ robo-components/DeploymentTrait.php
@@ -611,8 +611,9 @@ trait DeploymentTrait {
         throw new \Exception('The installation of the dependencies failed.');
       }
     }
-    else {
-      $this->_exec('sudo gem update');
+
+    if (file_exists('travis-key.enc')) {
+      throw new \Exception("The encrypted key (travis-key.enc) already exists. If you would like to override it, remove it first from the repository.");
     }
 
     $result = $this->taskExec('ssh-keygen -f travis-key -P ""')->run();
@@ -623,10 +624,6 @@ trait DeploymentTrait {
     $result = $this->taskExec('travis login --pro --github-token="' . $github_token . '"')->run();
     if ($result->getExitCode() !== 0) {
       throw new \Exception('The authentication with GitHub via Travis CLI failed.');
-    }
-
-    if (file_exists('travis-key.enc')) {
-      throw new \Exception("The encrypted key (travis-key.enc) already exists. If you would like to override it, remove it first from the repository.");
     }
 
     $result = $this->taskExec('travis encrypt-file travis-key --add --no-interactive --pro')
