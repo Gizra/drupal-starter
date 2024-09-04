596459fc2453fef550af323108218b75185edb68
Aron Novak <aron@gizra.com>
2024-09-04 13:41:06 +0200

    testdox everywhere

--- ci-scripts/test_phpunit.sh
+++ ci-scripts/test_phpunit.sh
@@ -4,6 +4,6 @@ set -e
 # -------------------------------------------------- #
 # Run PHPUnit tests
 # -------------------------------------------------- #
-ddev phpunit --do-not-cache-result --testdox || (ddev drush watchdog-show --count=1000 && exit 1)
+ddev phpunit --do-not-cache-result || (ddev drush watchdog-show --count=1000 && exit 1)
 
 exit 0
