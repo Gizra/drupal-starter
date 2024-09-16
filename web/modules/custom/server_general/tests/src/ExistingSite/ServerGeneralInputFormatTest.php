0ebea9fd0ebc0b420c158242c7728a558e5b724c
Milan Gurjanov <mgurjanov@gmail.com>
2024-09-17 01:44:33 +0200

    remove form from allowed tags in tests and add div/span with class

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralInputFormatTest.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralInputFormatTest.php
@@ -22,13 +22,11 @@ class ServerGeneralInputFormatTest extends ServerGeneralTestBase {
     $this->drupalGet('/node/add/news');
     $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
     $this->getSession()->getPage()->fillField('edit-title-0-value', 'Test Page' . time());
-    $this->getSession()->getPage()->fillField('edit-field-body-0-value', 'I can have a form: <form class="fun">, but not a script: <script></script>, as that would be way too dangerous. See https://owasp.org/www-community/attacks/xss/. <div class="danger-danger" onmouseover="javascript: whatafunction()">abc</div>');
+    $this->getSession()->getPage()->fillField('edit-field-body-0-value', 'I cannot have a script tag: <script></script>, as that would be way too dangerous. See https://owasp.org/www-community/attacks/xss/. <div class="danger-danger" onmouseover="javascript: whatafunction()">abc</div>');
     $this->getSession()->getPage()->selectFieldOption('edit-field-body-0-format--2', 'full_html');
     $this->click('#edit-submit');
     // <form> tag can be used.
-    $this->assertSession()->elementExists('css', '.node--type-news form');
     // The class attribute is preserved.
-    $this->assertSession()->elementExists('css', '.node--type-news form.fun');
     // <script> tag is eliminated.
     $this->assertSession()->elementNotExists('css', '.node--type-news script');
     // The onmouseover attribute is completely droppped.
