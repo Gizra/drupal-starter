7a735852c8de7a1c11f3b699e5bc5b0b493837c0
Amitai Burstein <amitai@gizra.com>
2024-11-01 17:51:18 +0200

    Wait for docker

---
+++ codespaces_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+wait_for_docker() {
+  # Loop until Docker responds, indicating it's ready
+  while true; do
+    docker ps > /dev/null 2>&1 && break
+    sleep 1
+  done
+  echo "Docker is ready."
+}
+
+wait_for_docker
+
+# Proceed with commands requiring Docker
+ddev composer install
+cp .ddev/config.local.yaml.example .ddev/config.local.yaml
+ddev restart
