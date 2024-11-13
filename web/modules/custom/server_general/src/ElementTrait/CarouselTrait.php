247ff73826d79a0077aea97f1cdc99806561d777
sonvir249 <39142-sonvir249@users.noreply.drupalcode.org>
2024-11-13 12:31:23 +0530

    Issue-490: Fixes Featured content missing on Arabic.

--- web/modules/custom/server_general/src/ElementTrait/CarouselTrait.php
+++ web/modules/custom/server_general/src/ElementTrait/CarouselTrait.php
@@ -41,13 +41,19 @@ trait CarouselTrait {
       return [];
     }
 
-    $elements = [];
     $elements[] = [
       '#theme' => 'server_theme_carousel',
       '#items' => $items,
       '#is_featured' => $is_featured,
       '#is_infinite' => $is_infinite,
     ];
+
+    $current_lang_code = \Drupal::languageManager()->getCurrentLanguage()->getId();
+    // RTL language codes.
+    $rtl_languages = ['ar', 'he', 'fa', 'ur'];
+    if (in_array($current_lang_code, $rtl_languages)) {
+      $elements[0]['#dir'] = 'rtl';
+    }
 
     if ($button) {
       $elements[] = $this->wrapTextCenter($button);
