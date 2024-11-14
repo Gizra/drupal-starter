26a298b388e1a4c623e83ddbfef321fbfe84227e
Aron Novak <aron@gizra.com>
2024-11-14 13:51:08 +0100

    fix rollbar config

--- ci-scripts/settings_rollbar.php
+++ ci-scripts/settings_rollbar.php
@@ -1,4 +1,4 @@
 $config["rollbar.settings"]["enabled"] = TRUE;
 $config["rollbar.settings"]["environment"] = "jep-rootone.travis-local";
 $config["rollbar.settings"]["log_level"] = [0,1,2,3,4];
-$config["rollbar.settings"]["access_token"] = "$ROLLBAR_SERVER_TOKEN";
+$config["rollbar.settings"]["access_token"] = getenv('ROLLBAR_SERVER_TOKEN');
