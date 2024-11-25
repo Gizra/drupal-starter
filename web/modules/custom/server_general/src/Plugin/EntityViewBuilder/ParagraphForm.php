e4b4b14989e4a158fe38e559ece545a173165aea
Milan Gurjanov <mgurjanov@gmail.com>
2024-11-26 00:20:07 +0100

    pr review changes

--- web/modules/custom/server_general/src/Plugin/EntityViewBuilder/ParagraphForm.php
+++ web/modules/custom/server_general/src/Plugin/EntityViewBuilder/ParagraphForm.php
@@ -2,10 +2,12 @@
 
 namespace Drupal\server_general\Plugin\EntityViewBuilder;
 
-use Drupal\Core\Entity\EntityFormBuilder;
+use Drupal\Core\Cache\CacheableMetadata;
 use Drupal\paragraphs\ParagraphInterface;
 use Drupal\pluggable_entity_view_builder\EntityViewBuilderPluginAbstract;
-use Drupal\server_general\FormsEmbedTrait;
+use Drupal\server_general\ElementWrapTrait;
+use Drupal\server_general\ProcessedTextBuilderTrait;
+use Drupal\server_general\TitleAndLabelsTrait;
 
 /**
  * The "Form" paragraph plugin.
@@ -18,14 +20,9 @@ use Drupal\server_general\FormsEmbedTrait;
  */
 class ParagraphForm extends EntityViewBuilderPluginAbstract {
 
-  use FormsEmbedTrait;
-
-  /**
-   * The form builder.
-   *
-   * @var \Drupal\Core\Entity\EntityFormBuilder
-   */
-  protected EntityFormBuilder $entityFormBuilder;
+  use ElementWrapTrait;
+  use ProcessedTextBuilderTrait;
+  use TitleAndLabelsTrait;
 
   /**
    * Build full view mode.
@@ -39,7 +36,45 @@ class ParagraphForm extends EntityViewBuilderPluginAbstract {
    *   Render array.
    */
   public function buildFull(array $build, ParagraphInterface $entity): array {
-    return $this->buildWebformEmbed($entity, $build, 'field_webform', 'field_title', 'field_description');
+
+    if (!$entity->hasField('field_webform') && $entity->get('field_webform')->isEmpty()) {
+      return [];
+    }
+
+    $webform_name = $entity->get('field_webform')->getValue()[0]['target_id'];
+    if (empty($webform_name)) {
+      return [];
+    }
+
+    $elements = [];
+    // Build the webform paragraph title.
+    if (!$entity->get('field_title')->isEmpty()) {
+      $elements[] = $this->buildParagraphTitle($this->getTextFieldValue($entity, 'field_title'));
+    }
+
+    // Build the webform paragraph description.
+    if (!$entity->get('field_description')->isEmpty()) {
+      $elements[] = $this->buildProcessedText($entity, 'field_description');
+    }
+
+    // Build the webform.
+    /** @var ?\Drupal\webform\WebformInterface $webform */
+    $webform = $this->entityTypeManager->getStorage('webform')->load($webform_name);
+    if (empty($webform) || !$webform->isOpen()) {
+      return [];
+    }
+    $element = $this->entityTypeManager->getViewBuilder('webform')->view($webform);
+    $elements[] = $element;
+
+    // Add cache dependencies.
+    CacheableMetadata::createFromRenderArray($build)
+      ->addCacheableDependency($webform)
+      ->applyTo($build);
+
+    $element = $this->wrapContainerVerticalSpacingBig($elements);
+    $build[] = $this->wrapContainerWide($element);
+
+    return $build;
   }
 
 }
