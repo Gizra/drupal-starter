1ba454d42616fb56b567eb6a034d666eaeaa47ac
Sonvir <choudharysonvir@gmail.com>
2024-12-10 17:16:56 +0530

    Issue-693: Disallow scheduler on locked pages (if scheduler module is enabled)  (#754)
    Co-authored-by: sonvir249 <39142-sonvir249@users.noreply.drupalcode.org>

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralNodeLandingPageTest.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralNodeLandingPageTest.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\Tests\server_general\ExistingSite;
 
+use Drupal\Core\Extension\ModuleInstallerInterface;
+use Drupal\node\Entity\NodeType;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -171,6 +173,75 @@ class ServerGeneralNodeLandingPageTest extends ServerGeneralNodeTestBase {
     // Check not locked node for anonymous.
     $this->drupalGet("/node/{$node->id()}/delete");
     $this->assertSession()->statusCodeEquals(Response::HTTP_FORBIDDEN);
+  }
+
+  /**
+   * Test schedular for locked page.
+   */
+  public function testSchedularOnLockedPage() {
+    // Install the scheduler module.
+    $module_installer = \Drupal::service('module_installer');
+    assert($module_installer instanceof ModuleInstallerInterface);
+    $module_installer->install(['scheduler']);
+
+    // Enable scheduler for Landing Page content type.
+    $node_type = NodeType::load('landing_page');
+    $node_type
+      ->setThirdPartySetting('scheduler', 'publish_enable', TRUE)
+      ->setThirdPartySetting('scheduler', 'unpublish_enable', TRUE)
+      ->save();
+    $node_type->save();
+
+    // Enable the scheduler fields in the default form display, mimicking
+    // what would be done if the entity bundle had been enabled via admin UI.
+    $this->container->get('entity_display.repository')
+      ->getFormDisplay('node', 'landing_page')
+      ->setComponent('publish_on', ['type' => 'datetime_timestamp_no_default'])
+      ->setComponent('unpublish_on', ['type' => 'datetime_timestamp_no_default'])
+      ->save();
+
+    // Login as admin.
+    $user = $this->createUser();
+    $user->addRole('administrator');
+    $user->save();
+    $this->drupalLogin($user);
+
+    // Create not locked page for admin.
+    $node = $this->createNode([
+      'title' => 'Testing not locked page',
+      'uid' => $user->id(),
+      'type' => 'landing_page',
+      'moderation_state' => 'published',
+    ]);
+
+    $node->setPublished()->save();
+
+    $this->drupalGet($node->toUrl('edit-form'));
+    $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
+
+    // Check schedular options exists.
+    $this->assertSession()->pageTextContains('Scheduling options');
+    $this->assertSession()->elementExists('css', '.scheduler-form');
+
+    // Make page locked.
+    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager */
+    $entity_type_manager = \Drupal::entityTypeManager();
+    /** @var \Drupal\config_pages\ConfigPagesStorage $config_pages_storage */
+    $config_pages_storage = $entity_type_manager->getStorage('config_pages');
+    /** @var \Drupal\Core\Entity\ContentEntityInterface|null $main_settings */
+    $main_settings = $config_pages_storage->load('main_settings');
+
+    $main_settings->get('field_locked_pages')->appendItem(['target_id' => $node->id()]);
+    $main_settings->save();
+
+    $this->drupalGet($node->toUrl('edit-form'));
+    $this->assertSession()->statusCodeEquals(Response::HTTP_OK);
+    // Check schedular options not exists.
+    $this->assertSession()->pageTextNotContains('Scheduling options');
+    $this->assertSession()->elementNotExists('css', '.scheduler-form');
+
+    $main_settings->get('field_locked_pages')->removeItem(1);
+    $main_settings->save();
   }
 
 }
