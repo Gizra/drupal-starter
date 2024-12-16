a0b87739a036104921f3d953bc5c3743357ebbe5
sonvir249 <39142-sonvir249@users.noreply.drupalcode.org>
2024-12-16 22:14:30 +0530

    issue-693: Fixes the phpstan failed testCase.

--- web/modules/custom/server_rollbar_test/tests/src/ExistingSite/ServerGeneralRollbarTest.php
+++ web/modules/custom/server_rollbar_test/tests/src/ExistingSite/ServerGeneralRollbarTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\server_rollbar_test\ExistingSite;
 
+use Drupal\Core\Extension\ModuleInstallerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
@@ -16,8 +17,15 @@ class ServerGeneralRollbarTest extends ExistingSiteBase {
    * Test admin role.
    */
   public function testAdministratorRole(): void {
+    // Install the scheduler module.
+    $module_installer = \Drupal::service('module_installer');
+    assert($module_installer instanceof ModuleInstallerInterface);
+    $module_installer->install(['server_rollbar_test']);
+
     $this->failOnPhpWatchdogMessages = FALSE;
     $users = \Drupal::entityTypeManager()->getStorage('user')->loadByProperties(['name' => 'AdminOne']);
+
+    /** @var \Drupal\user\Entity\User|false $user */
     $user = reset($users);
     $this->drupalLogin($user);
     $this->drupalGet('/admin/debug/rollbar-error-reporting');
