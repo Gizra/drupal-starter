5fe5e7e3b39436fcca58669cb4c3ba8f3ca5ddc8
Aron Novak <aron@gizra.com>
2024-12-18 14:51:37 +0100

    bump ddev

--- ci-scripts/install_ddev.sh
+++ ci-scripts/install_ddev.sh
@@ -7,7 +7,7 @@ else
   docker login --password "$DOCKER_PASSWORD" --username amitaibu
 fi
 
-DDEV_VERSION="v1.23.5"
+DDEV_VERSION="v1.24.1"
 
 if ! command -v ddev &>/dev/null; then
     echo "Installing ddev."
