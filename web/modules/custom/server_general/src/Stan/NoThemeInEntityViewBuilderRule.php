3de2f0a4152905f8ba7bbf68b497042682a32e42
Zsolt <zsolt.nagy@gizra.com>
2025-03-03 11:49:41 +0100

    Phpcs and phpstan fixes.

--- web/modules/custom/server_general/src/Stan/NoThemeInEntityViewBuilderRule.php
+++ web/modules/custom/server_general/src/Stan/NoThemeInEntityViewBuilderRule.php
@@ -4,27 +4,33 @@ declare(strict_types=1);
 
 namespace Drupal\server_general\Stan;
 
-use PhpParser\Node;
-use PhpParser\Node\Expr\Array_;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\ClassReflection;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
+use PhpParser\Node;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Scalar\String_;
 
 /**
+ * Checks if #theme is used in EntityViewBuilder classes.
+ *
  * @implements Rule<Array_>
  */
-class NoThemeInEntityViewBuilderRule implements Rule
-{
+class NoThemeInEntityViewBuilderRule implements Rule {
   private const ERROR_MESSAGE = "Use of '#theme' is not allowed in Pluggable entity view builder `EntityViewBuilder` classes";
 
-  public function getNodeType(): string
-  {
+  /**
+   * Gets the node type.
+   */
+  public function getNodeType(): string {
     return Array_::class;
   }
 
-  public function processNode(Node $node, Scope $scope): array
-  {
+  /**
+   * Processes the node.
+   */
+  public function processNode(Node $node, Scope $scope): array {
     if (!$this->isInEntityViewBuilder($scope)) {
       return [];
     }
@@ -32,42 +38,49 @@ class NoThemeInEntityViewBuilderRule implements Rule
     if ($this->containsThemeKey($node)) {
       return [
         RuleErrorBuilder::message(self::ERROR_MESSAGE)
-          ->line($node->getLine())
-          ->build()
+          ->line($node->getStartLine())
+          ->build(),
       ];
     }
 
     return [];
   }
 
-  private function isInEntityViewBuilder(Scope $scope): bool
-  {
+  /**
+   * Checks if class is extended from EntityViewBuilder.
+   */
+  private function isInEntityViewBuilder(Scope $scope): bool {
     $class = $scope->getClassReflection();
-    if ($class === null) {
-      return false;
+    if ($class === NULL) {
+      return FALSE;
     }
 
     return $this->isEntityViewBuilder($class);
   }
 
-  private function isEntityViewBuilder(ClassReflection $class): bool
-  {
+  /**
+   * Checks if class is subclass of EntityViewBuilder.
+   */
+  private function isEntityViewBuilder(ClassReflection $class): bool {
     return $class->isSubclassOf('Drupal\pluggable_entity_view_builder\EntityViewBuilder') ||
       $class->getName() === 'Drupal\pluggable_entity_view_builder\EntityViewBuilder';
   }
 
-  private function containsThemeKey(Array_ $node): bool
-  {
+  /**
+   * Checks if array contains '#theme' key.
+   */
+  private function containsThemeKey(Array_ $node): bool {
     foreach ($node->items as $item) {
-      if ($item === null) {
+      if ($item == NULL) {
         continue;
       }
 
-      if ($item->key instanceof Node\Scalar\String_ && $item->key->value === '#theme') {
-        return true;
+      if ($item->key instanceof String_ && $item->key->value === '#theme') {
+        return TRUE;
       }
     }
 
-    return false;
+    return FALSE;
   }
+
 }
