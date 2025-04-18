6fcd58d68ab62018f8e7bb10350645225a9580ad
Emircan ERKUL <develop@emircanerkul.com>
2025-04-18 15:10:03 +0300

    inject services

--- web/modules/custom/home_assignment/src/Plugin/EntityViewBuilder/NodeGroup.php
+++ web/modules/custom/home_assignment/src/Plugin/EntityViewBuilder/NodeGroup.php
@@ -5,8 +5,13 @@ namespace Drupal\home_assignment\Plugin\EntityViewBuilder;
 use Drupal\Component\Render\FormattableMarkup;
 use Drupal\Core\Url;
 use Drupal\node\NodeInterface;
-use Drupal\og\Og;
+use Drupal\og\MembershipManagerInterface;
 use Drupal\pluggable_entity_view_builder\EntityViewBuilderPluginAbstract;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityRepositoryInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Session\AccountInterface;
 
 /**
  * The "Group" paragraph plugin.
@@ -19,6 +24,62 @@ use Drupal\pluggable_entity_view_builder\EntityViewBuilderPluginAbstract;
  */
 class NodeGroup extends EntityViewBuilderPluginAbstract
 {
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The OG membership manager service.
+   *
+   * @var \Drupal\og\MembershipManagerInterface
+   */
+  protected $ogMembershipManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager,
+    AccountInterface $current_user,
+    EntityRepositoryInterface $entity_repository,
+    LanguageManagerInterface $language_manager,
+    MembershipManagerInterface $og_membership_manager
+  ) {
+    parent::__construct(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $entity_type_manager,
+      $current_user,
+      $entity_repository,
+      $language_manager
+    );
+    $this->currentUser = $current_user;
+    $this->ogMembershipManager = $og_membership_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('current_user'),
+      $container->get('entity.repository'),
+      $container->get('language_manager'),
+      $container->get('og.membership_manager')
+    );
+  }
 
   /**
    * Build full view mode.
@@ -33,8 +94,8 @@ class NodeGroup extends EntityViewBuilderPluginAbstract
    */
   public function buildFull(array $build, NodeInterface $entity): array
   {
-    $user = \Drupal::currentUser();
-    $is_member = Og::isMember($entity, $user);
+    $user = $this->currentUser;
+    $is_member = $this->ogMembershipManager->isMember($entity, $user->id());
 
     if (!$is_member) {
       $url = Url::fromRoute('og.subscribe', [
@@ -54,6 +115,5 @@ class NodeGroup extends EntityViewBuilderPluginAbstract
 
     return $build;
   }
-
 
 }
