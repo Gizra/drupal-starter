5a8c4351965e015a7b6458d43151d537b05b501f
Bysa Boro <bysa@fabbdev.com>
2025-05-22 18:57:33 +0100

    WIP: Layout of header and search form

--- web/themes/custom/server_theme/src/js/scripts.js
+++ web/themes/custom/server_theme/src/js/scripts.js
@@ -3,7 +3,7 @@
  */
 
 
-(function ($, Drupal) {
+(function ($, Drupal, once) {
   'use strict';
 
   /**
@@ -20,5 +20,45 @@
       anchors.add();
     },
   };
+  /**
+   * Actions with desktop search.
+   */
+  Drupal.behaviors.serverThemeSearchToggle = {
+    attach: function (context, settings) {
+      const $toggle = $(once('server-theme-search-toggle', '.js-search-toggle', context));
+      if (!$toggle.length) {
+        return;
+      }
 
-})(jQuery, Drupal);
+      const $body = $(document.body);
+
+      // Disable the hidden form inputs so they don't get selected when a user's
+      // tabbing through the page.
+      const $form = $('.js-search-form');
+      const $inputs = $form.find('input');
+
+      $inputs.prop('disabled', 'disabled');
+      // Shows search textfield for desktop.
+      $toggle.on('click', function (event) {
+        event.stopPropagation();
+        // Enable the inputs.
+        $inputs.prop('disabled', false);
+        $body.addClass('js-search-form-open');
+        $form.find('input[name="key"]').focus();
+      });
+
+      $form.on('click', function(event) {
+        event.stopPropagation();
+      });
+
+      $body.on('click', function(event) {
+        $body.removeClass('js-search-form-open');
+
+        if ($(this).hasClass('js-search-form-open')) {
+          $(this).removeClass('js-search-form-open');
+        }
+      });
+    },
+  };
+
+})(jQuery, Drupal, once);
