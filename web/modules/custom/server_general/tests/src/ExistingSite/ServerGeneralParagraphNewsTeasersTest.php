cc43b9c445440042812c9775e96c833b3fb3fa5f
Bernard Munslow <bernardmunslow@gmail.com>
2025-06-01 14:26:10 +0200

    Fix smart trim settings in processed text (#849)

--- web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralParagraphNewsTeasersTest.php
+++ web/modules/custom/server_general/tests/src/ExistingSite/ServerGeneralParagraphNewsTeasersTest.php
@@ -2,10 +2,14 @@
 
 namespace Drupal\Tests\server_general\ExistingSite;
 
+use Drupal\Tests\server_general\Traits\ParagraphCreationTrait;
+
 /**
  * Test 'views' paragraph type.
  */
 class ServerGeneralParagraphNewsTeasersTest extends ServerGeneralParagraphTestBase {
+
+  use ParagraphCreationTrait;
 
   /**
    * {@inheritdoc}
@@ -27,7 +31,42 @@ class ServerGeneralParagraphNewsTeasersTest extends ServerGeneralParagraphTestBa
   public function getOptionalFields(): array {
     return [
       'field_title',
+      'field_body',
     ];
+  }
+
+  /**
+   * Test render of the paragraph.
+   */
+  public function testRender() {
+    $paragraph = $this->createParagraph([
+      'type' => $this->getEntityBundle(),
+    ]);
+
+    // Add a Landing page, and reference the Paragraph.
+    $landing_page_node = $this->createNode([
+      'type' => 'landing_page',
+      'moderation_state' => 'published',
+      'field_paragraphs' => [
+        $paragraph,
+      ],
+    ]);
+
+    // Create node.news with allowed and disallowed html tags in field_body.
+    // @see Drupal\server_general\ProcessedTextBuilderTrait
+    $body = 'The wrapping h3 tag should be stripped but <strong>this strong one not.</strong>';
+
+    $this->createNode([
+      'title' => 'Test News',
+      'type' => 'news',
+      'field_body' => '<h3>' . $body . '</h3>',
+      'moderation_state' => 'published',
+    ]);
+
+    $this->drupalGet($landing_page_node->toUrl());
+    $this->assertSession()->elementNotExists('css', '.node--type-news.node--view-mode-teaser .field--name-field-body h3');
+    $this->assertSession()->elementExists('css', '.node--type-news.node--view-mode-teaser .field--name-field-body strong');
+    $this->assertSession()->pageTextContains($body);
   }
 
 }
