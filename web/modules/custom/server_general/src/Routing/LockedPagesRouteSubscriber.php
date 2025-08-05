ea1026ccab04e0c719fc5863f5a56910d749df44
Bysa Boro <bysa@fabbdev.com>
2025-08-05 18:22:12 +0100

    Always add cache tags of config_pages

--- web/modules/custom/server_general/src/Routing/LockedPagesRouteSubscriber.php
+++ web/modules/custom/server_general/src/Routing/LockedPagesRouteSubscriber.php
@@ -78,10 +78,10 @@ final class LockedPagesRouteSubscriber extends RouteSubscriberBase {
   public function access(AccountInterface $account, NodeInterface $node): AccessResultInterface {
     // Get the list of bundles that can be restricted.
     $bundles = $this->lockedPagesService->getReferencedBundles();
+    $main_settings = $this->lockedPagesService->getMainSettings();
+    $cache_tags = $main_settings->getCacheTags();
     // If node is locked, we don't allow accesing the delete page at all.
     if (in_array($node->getType(), $bundles) && $this->lockedPagesService->isNodeLocked($node)) {
-      $main_settings = $this->lockedPagesService->getMainSettings();
-      $cache_tags = $main_settings->getCacheTags();
       return AccessResult::forbidden()->addCacheableDependency($node)->addCacheTags($cache_tags);
     }
 
@@ -90,7 +90,9 @@ final class LockedPagesRouteSubscriber extends RouteSubscriberBase {
     // owner.
     $has_delete_permission = $account->hasPermission("delete any {$node->bundle()} content") || ($node->getOwnerId() === $account->id() && $account->hasPermission("delete own {$node->bundle()} content"));
     $result = AccessResult::allowedIf($has_delete_permission);
-    $result->addCacheableDependency($node);
+    $result->addCacheableDependency($node)
+      ->addCacheTags($cache_tags);
+
     return $result;
   }
 
