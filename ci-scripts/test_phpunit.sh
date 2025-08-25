87d63451b5a5e87210ca99a8e47981c9b27f908c
Aron Novak <AronNovak@users.noreply.github.com>
2025-08-25 09:01:31 +0200

    Simplify `phpunit` execution logic at Travis (#888)

--- ci-scripts/test_phpunit.sh
+++ ci-scripts/test_phpunit.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-# -------------------------------------------------- #
-# Run PHPUnit tests
-# -------------------------------------------------- #
-ddev phpunit --do-not-cache-result --exclude-group=Rollbar || (ddev drush watchdog-show --count=1000 && exit 1)
-
-exit 0
+ddev phpunit --do-not-cache-result --exclude-group=Rollbar
