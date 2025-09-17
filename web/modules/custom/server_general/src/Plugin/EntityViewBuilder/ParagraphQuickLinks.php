5bc91c9cf5821b4af56e35dd22e936c01de1afa8
Aron Novak <aron@gizra.com>
2025-09-17 12:17:57 +0200

    more cleanup

--- web/modules/custom/server_general/src/Plugin/EntityViewBuilder/ParagraphQuickLinks.php
+++ web/modules/custom/server_general/src/Plugin/EntityViewBuilder/ParagraphQuickLinks.php
@@ -7,8 +7,6 @@ namespace Drupal\server_general\Plugin\EntityViewBuilder;
 use Drupal\paragraphs\ParagraphInterface;
 use Drupal\pluggable_entity_view_builder\EntityViewBuilderPluginAbstract;
 use Drupal\server_general\ProcessedTextBuilderTrait;
-use Drupal\server_general\ThemeTrait\CardThemeTrait;
-use Drupal\server_general\ThemeTrait\ElementWrapThemeTrait;
 use Drupal\server_general\ThemeTrait\QuickLinksThemeTrait;
 
 /**
