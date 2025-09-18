f4606300912fb6c6c27f63651f894ff92c55b202
Zsolt Nagy <balagan73@gmail.com>
2025-09-18 13:09:48 +0200

    Add automatic updater robo component. (#867)

---
+++ robo-components/AutoUpdateTrait.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace RoboComponents;
+
+use Drupal\update\UpdateManagerInterface;
+
+/**
+ * Automatically update core and modules.
+ */
+trait AutoUpdateTrait {
+
+  /**
+   * Update drupal core and modules.
+   *
+   * @throws \Exception
+   */
+  public function updateModules() {
+    if (!\Drupal::moduleHandler()->moduleExists('update')) {
+      throw new \Exception("The update module should be installed in order to run this command.");
+    }
+    $this->say("Update module is installed, checking status of projects.");
+    $this->say("In case of some errors manually loading /admin/reports/updates can help.");
+
+    if (!($available = update_get_available(TRUE))) {
+      $this->say("Cannot fetch info about the releases.");
+    }
+    \Drupal::moduleHandler()->loadInclude('update', 'compare.inc');
+    $data = update_calculate_project_data($available);
+    foreach ($data as $project) {
+      if (!isset($project['recommended'])) {
+        $this->yell('No recommended version is set for ' . $project['name']);
+        continue;
+      }
+      // Drupal core is simply called drupal.
+      $package = $project['name'] == 'drupal' ? 'core-*' : $project['name'];
+      if ($project['status'] == UpdateManagerInterface::CURRENT && $project['existing_version'] === $project['recommended']) {
+        $this->say($project['name'] . ' is up-to-date.');
+        // No need to update.
+        continue;
+      }
+      $this->say('Updating ' . $package . ' to version ' . $project['recommended'] . '.');
+      $exit_code = $this->taskExec("composer update 'drupal/" . $package . ":^" . $project['recommended'] . "' -W")
+        ->printOutput(TRUE)
+        ->run()
+        ->getExitCode();
+      if ($exit_code !== 0) {
+        throw new \Exception("There was an error updating " . $package . " to version " . $project['recommended']);
+      }
+
+      $current_branch = trim(`git symbolic-ref --short HEAD`);
+
+      // Don't commit to master/main branch.
+      if (in_array($current_branch, ['master', 'main'], TRUE)) {
+        throw new \Exception("This command cannot be run on the {$current_branch} branch.");
+      }
+
+      // Update successful, add composer.lock to staging area,
+      // then commit it.
+      $this->taskExec("git add composer.lock")->printOutput(TRUE)->run();
+      $git_command = "git commit -m 'Update " . $package . ' to ' . $project['recommended'] . "'";
+      $this->taskExec($git_command)->printOutput(TRUE)->run();
+    }
+  }
+
+}
