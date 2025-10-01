6b5f0abf40ba3abb1838cb22bfa889570ab73cf4
Aron Novak <aron@gizra.com>
2025-10-01 12:52:21 +0200

    Revert "Port phpcs parallel processing support (#900)"
    This reverts commit 969033b25266bb76560450703b52f43df11fa44e.

--- robo-components/PhpcsTrait.php
+++ robo-components/PhpcsTrait.php
@@ -41,26 +41,16 @@ trait PhpcsTrait {
 
     $error_code = NULL;
 
-    // Use GNU parallel for faster execution.
-    foreach ($commands as $command) {
-      // Build all command combinations for parallel execution.
-      $command_list = [];
-      foreach ($directories as $directory) {
-        foreach ($standards as $standard) {
-          $arguments = "--standard=$standard -p --ignore=" . self::$themeName . "/dist,node_modules,server_default_content/content --colors --extensions=php,module,inc,install,test,profile,theme,css,yaml,yml,txt,md";
-          $command_list[] = "cd web && ../vendor/bin/$command $directory $arguments";
+    foreach ($directories as $directory) {
+      foreach ($standards as $standard) {
+        $arguments = "--parallel=8 --standard=$standard -p --ignore=" . self::$themeName . "/dist,node_modules,server_default_content/content --colors --extensions=php,module,inc,install,test,profile,theme,css,yaml,yml,txt,md";
+
+        foreach ($commands as $command) {
+          $result = $this->_exec("cd web && ../vendor/bin/$command $directory $arguments");
+          if (empty($error_code) && !$result->wasSuccessful()) {
+            $error_code = $result->getExitCode();
+          }
         }
-      }
-
-      // Use GNU parallel to execute all commands in parallel.
-      $commands_file = tempnam(sys_get_temp_dir(), 'phpcs_commands');
-      file_put_contents($commands_file, implode("\n", $command_list));
-
-      $result = $this->_exec("parallel -j+0 --halt now,fail=1 < $commands_file");
-      unlink($commands_file);
-
-      if (empty($error_code) && !$result->wasSuccessful()) {
-        $error_code = $result->getExitCode();
       }
     }
 
